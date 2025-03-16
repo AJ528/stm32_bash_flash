12020993d2e9e0b01544829fce1be5b7e614ae87
ratman528 <adevries528@gmail.com>
2025-03-15 21:45:30 -0500

    added bash script

---
+++ flash.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -x
+
+POSITIONAL_ARGS=()
+
+device="/dev/ttyUSB0"
+baud="115200"
+reset_option=""
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -d)
+      device="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -R)
+      reset_option="$1"
+      shift
+      ;;
+    -*)
+      echo "Unknown option" $1
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+if [[ -n $1 ]]; then
+
+
+  if [ "$1" = "erase" ]; then
+    stm32flash -b $baud -o $device
+  else
+    echo "device = $device"
+
+    start_addr=$(arm-none-eabi-readelf -l $1 | head -n 8 | tail -n 1 | xargs | cut -d " " -f 4) 
+    echo "start address = $start_addr"
+
+    temp_file=$(mktemp)
+    arm-none-eabi-objcopy --input-target elf32-littlearm --output-target binary $1 $temp_file
+
+    stm32flash -b $baud -w $temp_file -v $reset_option -S $start_addr $device
+
+    rm $temp_file
+  fi
+else
+  echo "ERROR: must specify \"erase\" or provide path to elf file"
+fi
+
+
+
