e798643f2c3abd88dadcbf7d9979c600903d0d41
ratman528 <adevries528@gmail.com>
2025-03-15 21:52:34 -0500

    added comments

--- flash.sh
+++ flash.sh
@@ -38,14 +38,18 @@ if [[ -n $1 ]]; then
   else
     echo "device = $device"
 
+    # read the ELF to determine where the binary file should be loaded
     start_addr=$(arm-none-eabi-readelf -l $1 | head -n 8 | tail -n 1 | xargs | cut -d " " -f 4) 
     echo "start address = $start_addr"
 
+    # create a temporary file and store the binary output there
     temp_file=$(mktemp)
     arm-none-eabi-objcopy --input-target elf32-littlearm --output-target binary $1 $temp_file
 
+    # load the binary file onto the microcontroller
     stm32flash -b $baud -w $temp_file -v $reset_option -S $start_addr $device
 
+    # remove the temp file
     rm $temp_file
   fi
 else
