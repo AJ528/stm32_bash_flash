e7c9fb6e61b1a5195103b7d0067eec81fae576d2
AJ528 <adevries528@gmail.com>
2025-03-16 23:49:40 -0500

    removed bash script and replaced it with python script

---
+++ flash.py
@@ -0,0 +1,76 @@
+
+import argparse
+import subprocess
+from pathlib import Path
+import tempfile
+
+BAUD_RATE = 115200
+
+def run(args):
+  with subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as process:
+    for line in process.stdout:
+      print(line.decode('utf8'), end="")
+  return process.returncode
+
+def get_elf_start_addr(elf_path):
+  start_addr = None
+  readelf_str = "arm-none-eabi-readelf -l " + str(elf_path)
+  with subprocess.Popen(readelf_str.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as process:
+    for line_num, line in enumerate(process.stdout):
+      if(line_num == 7):
+        tokens = line.decode('utf8').split()
+        start_addr = tokens[3]
+        return start_addr
+  raise OSError("unable to parse elf file")
+
+def create_binary(elf_path):
+  temp_file = tempfile.NamedTemporaryFile(delete_on_close=False, delete=False)
+  print("temp_file name =", str(temp_file.name))
+  create_bin_str = "arm-none-eabi-objcopy --input-target elf32-littlearm --output-target binary " + str(elf_path) + " " + str(temp_file.name)
+  run(create_bin_str.split())
+  return Path(temp_file.name)
+
+def erase_MCU(device_path):
+  erase_str = "stm32flash -b "+ str(BAUD_RATE) + " -o " + device_path
+  run(erase_str.split())
+
+def flash_MCU(elf_path, reset_arg, device_path):
+  start_address = get_elf_start_addr(elf_path)
+  print(f"start address is {start_address}")
+  bin_file = create_binary(elf_path)
+
+  flash_str = "stm32flash -b " + str(BAUD_RATE) + " -w " + str(bin_file) + \
+              " -v -f " + reset_arg + " -S " + start_address + " " + device_path
+  run(flash_str.split())
+  bin_file.unlink()
+
+def parse_args():
+  # Instantiate the parser
+  parser = argparse.ArgumentParser()
+  parser.add_argument("action", help = "either \"erase\" or the path to an ELF file")
+  parser.add_argument("-d", help = "set the device path (default = /dev/ttyUSB0)", dest = "device_path", default = "/dev/ttyUSB0")
+  parser.add_argument("-R", help = "reset device at exit", dest = "reset_device", action = 'store_true')
+
+  args = parser.parse_args()
+  return args
+
+def main():
+
+  args = parse_args()
+
+  if(args.reset_device):
+    reset_arg = "-R"
+  else:
+    reset_arg = ""
+
+  print(f"device = {args.device_path}")
+
+  if (args.action == "erase"):
+    erase_MCU(args.device_path)
+  else:
+    elf_path = Path(args.action).resolve(strict=True)
+    flash_MCU(elf_path, reset_arg, args.device_path)
+
+
+if __name__ == "__main__":
+  main()
